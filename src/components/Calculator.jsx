24a0b315ea96901fe82a82d81a72651b3a6fb5d7
Lornot <veishen19@gmail.com>
2017-03-14 23:33:14 +0200

    init

---
+++ src/components/Calculator.jsx
@@ -0,0 +1,19 @@
+var React = require('react');
+
+var Calculator = React.createClass({
+    render: function () {
+        return (
+            <div className="container">
+                <div className="row">
+                    div.
+                    <div className="col-md-offset-3"></div>
+                    <div className="col-md-offset-6">
+                        <h1>Simple calculator</h1>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+});
+
+module.exports = Calculator;
